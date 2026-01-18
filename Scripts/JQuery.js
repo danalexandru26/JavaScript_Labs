6b7f53fb4646ed67d2a39edee753f61f6d858d79
Alexandru <dan.bulzan@student.upt.ro>
2026-01-18 15:15:33 +0200

    jquery set and get done

--- Scripts/JQuery.js
+++ Scripts/JQuery.js
@@ -8,3 +8,18 @@ $(document).ready(function () {
         });
     });
 });
+
+$(document).ready(function () {
+    console.log("jQuery has loaded!");
+    $("#paraTest").click(function () {
+        alert("The paragraph has the following text: " + $("#paraTest").text());
+    });
+});
+
+$(document).ready(function () {
+    $("#genericID").click(function () {
+        $("#genericChild").text(function (id, oldText) {
+            return oldText + "is deprecated";
+        });
+    });
+});
